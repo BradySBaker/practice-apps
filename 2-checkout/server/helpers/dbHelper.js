3053b7b3ef068053d5359ac8e459971b14ca0a7f
BradySBaker <bradysamuelbaker@gmail.com>
2023-02-16 13:49:38 -0800

    Databse is updated at session id for each form

--- 2-checkout/server/helpers/dbHelper.js
+++ 2-checkout/server/helpers/dbHelper.js
@@ -11,26 +11,61 @@ var getResponse = (session, cb) => {
 	});
 }
 
+var createInsertQuery = (data, sessionId) => {
+	var columns = 'sessionId';
+	var values = `'${sessionId}'`;
+	for (var key in data) {
+			columns += `, ${key}`
+			values += `, '${data[key]}'`;
+		}
+	var query = `INSERT INTO responses (${columns}) VALUES (${values})`
+	return query;
+}
+
+var createUpdateQuery = (data, sessionId) => {
+	var setQuery;
+	for (var key in data) {
+		if (!setQuery) {
+			setQuery = `${key} = '${data[key]}'`
+		} else {
+			setQuery += `, ${key} = '${data[key]}'`
+		}
+	}
+	var query = `UPDATE responses SET ${setQuery} WHERE sessionId = '${sessionId}'`
+	return query;
+}
+
 var saveResponse = (session, data, cb) => {
-	getResponse(session, (err, data) => {
-		console.log(data);
+	getResponse(session, (err, responseData) => {
+		console.log(responseData);
+		if (err) {
+			cb(err);
+			return;
+		}
+		var columns;
+		var values;
+		var query;
+		if (responseData.length === 0) {
+			if (data.name) {
+				value = `'${session}'`
+				query = createInsertQuery(data, session);
+			} else {
+				cb('Missing form');
+				return;
+			}
+		} else {
+			query = createUpdateQuery(data, session);
+		}
+
+		db.query(query, (err) => {
+			if (err) {
+				console.log(err);
+				cb(err);
+			} else {
+				cb(null);
+			}
+		});
 	});
-	// var columns = 'sessionId';
-	// var values = `'${session}'`
-	// for (var key in data) {
-	// 	columns += `, ${key}`
-	// 	values += `, '${data[key]}'`;
-	// }
-	// console.log(columns, `values ${values}`);
-	// var query = `INSERT INTO responses (${columns}) VALUES (${values})`
-	// db.query(query, (err) => {
-	// 	if (err) {
-	// 		console.log(err);
-	// 		cb(err);
-	// 	} else {
-	// 		cb(null);
-	// 	}
-	// });
 };
 
 module.exports.saveResponse = saveResponse;
